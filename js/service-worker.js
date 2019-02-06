da512532d153bfd9f0d99c3c9880f8e3565ee026
Rutu Dave <47212587+RutuDave@users.noreply.github.com>
2019-02-06 17:10:14 +0530

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,11 +1,14 @@
-if ('serviceWorker' in navigator) {
-	  window.addEventListener('load', function() {
-	    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-	      // Registration was successful
-	      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	    }, function(err) {
-	      // registration failed :(
-	      console.log('ServiceWorker registration failed: ', err);
-	    });
-	  });
+if ('serviceWorker' in navigator) 
+{
+	navigator.serviceWorker.register('/ServiceWorker.js').then(function(response) 
+	{
+	    
+	    // Service worker registration done
+	    console.log('Registration Successful', response);
+	}, 
+	function(error) 
+	{
+	    // Service worker registration failed
+	    console.log('Registration Failed', error);
 	}
+}
