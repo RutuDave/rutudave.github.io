19da1a583fc5458611448c7f274d994a408021a7
Rutu Dave <47212587+RutuDave@users.noreply.github.com>
2019-02-06 18:13:51 +0530

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,16 +1,4 @@
-if ('serviceWorker' in navigator) 
-{
-    navigator.serviceWorker.register('js/service-worker.js').then(function(registration) 
-    {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, 
-    function(err) 
-    {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-    });
-}
+
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
